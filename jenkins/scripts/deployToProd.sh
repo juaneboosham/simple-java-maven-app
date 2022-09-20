b22d12d19a16054bd87cb4db34dfa66885ed7a64
longzhan.huang <juaneboosham@gmail.com>
2022-09-21 00:03:43 +0800

    add deployToProd.sh

--- jenkins/scripts/deployToProd.sh
+++ jenkins/scripts/deployToProd.sh
@@ -9,7 +9,7 @@ REMOTE_HOST=ec2-user@54.90.244.6
 
 echo ENV
 
-ssh -i /etc/firstECC.pem -o "StrictHostKeyChecking no" $REMOTE_HOST <<remotessh
+ssh -i /etc/firstECC.pem -o "StrictHostKeyChecking no" -t $REMOTE_HOST <<remotessh
 docker run $REMOTE_TAG
 exit
 remotessh
